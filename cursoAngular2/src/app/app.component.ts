cc77c03b4ec2f61b5b0b37ec7806c1c99cd0913f
Juan Antonio González Cano <jantoniogc@gmail.com>
2024-02-28 08:55:05 +0000

    asdf

--- cursoAngular2/src/app/app.component.ts
+++ cursoAngular2/src/app/app.component.ts
@@ -1,4 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Directive, HostListener } from '@angular/core';
+
+@Directive({selector: 'button[counting]'})
+class CountClicks {
+  numberOfClicks = 0;
+
+  @HostListener('click', ['$event.target'])
+  onClick(btn: any) {
+    console.log('button', btn, 'number of clicks:', this.numberOfClicks++);
+ }
+}
 
 @Component({
   selector: 'app-root',
@@ -19,5 +29,20 @@ export class AppComponent {
 
   getFecha(e: Date) {
     this.fecha = e
+  }
+
+  @HostListener('click', ["$event"]) 
+  onMouseEnter(ev: Event) {
+    alert("You just hovered over a paragraph");
+  }
+
+  counter = 0;
+  @HostListener('window:keydown', ['$event'])
+  handleKeyDown(event: KeyboardEvent) {
+    console.log(event);
+    this.counter++;
+  }
+  resetCounter() {
+    this.counter = 0;
   }
 }
